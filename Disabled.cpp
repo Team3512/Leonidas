74c473deffa3a608f42fb17a4f3c67f9be04e960
Tyler Veness <calcmogul@gmail.com>
2012-10-07 15:50:55 -0700

    Added joystick button toggling to Disabled.cpp so those button states would update on the DriverStationDisplay during Disabled mode

--- Disabled.cpp
+++ Disabled.cpp
@@ -6,12 +6,41 @@
 
 #include <Timer.h>
 #include "OurRobot.hpp"
+#include "ButtonTracker.hpp"
 
 void OurRobot::Disabled() {
     mainCompressor.Stop();
 
+    ButtonTracker driveStick1Buttons( 1 );
+    ButtonTracker driveStick2Buttons( 2 );
+    ButtonTracker turretStickButtons( 3 );
+
     while ( IsDisabled() ) {
         DS_PrintOut();
+
+        // update "new" value of joystick buttons
+       driveStick1Buttons.updateButtons();
+       driveStick2Buttons.updateButtons();
+       turretStickButtons.updateButtons();
+
+       /* ===================== AutoAim ===================== */
+       if ( turretStickButtons.releasedButton( 3 ) ) {
+           isAutoAiming = !isAutoAiming; // toggles autoAim
+       }
+       /* =================================================== */
+
+       /* ============== Toggle Shooter Motors ============== */
+       // turns shooter on/off
+       if ( turretStickButtons.releasedButton( 1 ) ) { // if released trigger
+           isShooting = !isShooting;
+       }
+
+       // toggle manual RPM setting vs setting with encoder input
+       if ( turretStickButtons.releasedButton( 2 ) ) {
+           shooterIsManual = !shooterIsManual;
+       }
+       /* =================================================== */
+
         Wait( 0.1 );
     }
 }
