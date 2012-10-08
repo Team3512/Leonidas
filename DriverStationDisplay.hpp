992bfdc54832d73732d1da12b3a8ad16b849048e
Tyler Veness <calcmogul@gmail.com>
2012-10-07 23:38:52 -0700

    Added comment section at beginning of DriverStationDisplay.hpp describing how to use the class

--- DriverStationDisplay.hpp
+++ DriverStationDisplay.hpp
@@ -7,6 +7,25 @@
 #ifndef DRIVER_STATION_DISPLAY_HPP
 #define DRIVER_STATION_DISPLAY_HPP
 
+/* This class allows you to pack data into an SFML packet and send it to an
+ * application on the DriverStation that displays it in a GUI.
+ *
+ * USAGE:
+ * 1) Call DriverStationDisplay::getInstance() to create an instance of this
+ *    class
+ * 2) Call clear() on the pointer to empty the packet before adding new data
+ * 3) Add new data with the << operator (e.g. *dsPtr << 4.f; *dsPtr << myVar;)
+ * 4) After all data is packed, call sendToDS() to send the data to the Driver
+ *    Station
+ *
+ * Extract the received packet on the DriverStation with the >> operator like
+ * any other SFML packet.
+ *
+ * Note: Make sure that the application on the Driver Station extracts the same
+ *       types from the received packet in the same order in which you inserted
+ *       them.
+ */
+
 #include "SFML/Network/IpAddress.hpp"
 #include "SFML/Network/Packet.hpp"
 #include "SFML/Network/UdpSocket.hpp"
