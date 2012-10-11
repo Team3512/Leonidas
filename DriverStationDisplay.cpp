1b13914d122014c27ac873d73a43cfc9f3b48389
Tyler Veness <calcmogul@gmail.com>
2012-10-10 18:12:43 -0700

    Made DriverStationDisplay send data to two computers at once for data collection purposes

--- DriverStationDisplay.cpp
+++ DriverStationDisplay.cpp
@@ -18,6 +18,8 @@ DriverStationDisplay* DriverStationDisplay::getInstance() {
 
 void DriverStationDisplay::sendToDS() {
     socket.send( *static_cast<sf::Packet*>(this) , dsIP , dsPort );
+    // TODO remove the second send before competition
+    socket.send( *static_cast<sf::Packet*>(this) , sf::IpAddress( "10.35.12.42" ) , dsPort );
 }
 
 DriverStationDisplay::DriverStationDisplay( sf::IpAddress IP , unsigned short portNumber ) : dsIP( IP ) , dsPort( portNumber ) {
