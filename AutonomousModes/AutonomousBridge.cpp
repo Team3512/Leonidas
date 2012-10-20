f9a4ab541c4b637eb7181b885b442450a67df477
Tyler Veness <calcmogul@gmail.com>
2012-10-20 16:04:08 -0700

    Added AutonomousBridge.cpp to be implemented later

---
+++ AutonomousModes/AutonomousBridge.cpp
@@ -0,0 +1,12 @@
+//=============================================================================
+//File Name: AutonomousBridge.cpp
+//Description: Drives to Coopertition bridge and tips it to our side
+//Author: FRC Team 3512, Spartatroniks
+//=============================================================================
+
+#include "../OurRobot.hpp"
+
+void OurRobot::Autonomous() {
+    while ( IsEnabled() && IsAutonomous() ) {
+    }
+}
