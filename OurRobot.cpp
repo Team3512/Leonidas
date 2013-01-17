fe2f37bec3dec6a03899b57916f544b44a814827
Tyler Veness <calcmogul@gmail.com>
2013-01-17 14:12:49 -0800

    Changed name of settings file

--- OurRobot.cpp
+++ OurRobot.cpp
@@ -14,7 +14,7 @@ float ScaleZ( Joystick& stick) {
 }
 
 OurRobot::OurRobot() :
-    Settings( "/ni-rt/system/IPSettings.txt" ),
+    Settings( "/ni-rt/system/RobotSettings.txt" ),
 
     mainCompressor( 1 , 6 ),
     mainDrive( 3 , 4 , 1 , 2 ),
